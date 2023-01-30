a45e045e040366cd48a44121582226d4b769dadc
ucdo <phpsarc@gmail.com>
2023-01-30 09:06:27 +0800

    feat: ues laravel  framework

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is a ✨ _special_ ✨ repository:
 
 - 🌱 I’m currently learning PHP and C
 - 🤔 I’m looking for help with English and Psychology
-- 💬 Ask me about ucdo [ukudo]
+- 💬 Call me about ucdo [ukudo]
 - 📫 How to reach me: phpsarc@gmail.com
 - 😄 Pronouns: hard working
 - 🐊 Say to me: do not limit yourself
---
+++ project/tests/Feature/ExampleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function testTheApplicationReturnsASuccessfulResponse()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
